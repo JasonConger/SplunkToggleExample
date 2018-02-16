80e3b0063e0bb45f79deef185bca5757eca195b6
Jason Conger <jconger@splunk.com>
2018-02-16 14:15:54 -0600

    Updated to work with Splunk 7.x

--- ToggleExample/appserver/static/toggle.js
+++ ToggleExample/appserver/static/toggle.js
@@ -8,33 +8,39 @@ require(['splunkjs/mvc/simplexml/ready!'], function(){
         // The splunkjs/ready loader script will automatically instantiate all elements
         // declared in the dashboard's HTML.
 	
-	// ************************************************************
-	// This fuction toggles the visibility and height of an element
-	// and is reusable.
-	// ************************************************************
-	function toggle(button, target) {
-	    
-	    if(target.css("visibility") == "hidden" ) {
-		button.attr("src", "/static/app/ToggleExample/collapse.png");
-		target.css({
-		    "height": "auto",
-		    "visibility": "visible"});
-	    }
-	    else
-	    {
-		button.attr("src", "/static/app/ToggleExample/expand.png");
-		target.css({
-		    "height": "0px",
-		    "visibility": "hidden"});
-	    }
-	}
-	
-        // Use jQuery to add the toggle button the the element we want
-        $("#single").find("h3").after('<img id="imgToggle" src="/static/app/ToggleExample/expand.png" style="float: right; padding-right: 15px; cursor: pointer" />');
-	
-	// Setup the click handler for the toggle button
-	$("#imgToggle").click(function(){
-	    toggle($(this), $(".dashboard-row2"));
-        });
+		// ************************************************************
+		// This fuction toggles the visibility and height of an element
+		// and is reusable.
+		// ************************************************************
+		function toggle(button, target) {
+			
+			if(target.css("height") == "0px" ) {
+				button.attr("src", "/static/app/ToggleExample/collapse.png");
+				target.css({
+					"height": "auto",
+					"overflow": "visible"
+				});
+			}
+			else
+			{
+				button.attr("src", "/static/app/ToggleExample/expand.png");
+				target.css({
+					"height": "0px",
+					"overflow": "hidden"
+				});
+			}
+		}
+			
+		// Setup the click handlers for the toggle buttons
+		$("#imgToggle1").click(function(){
+			toggle($(this), $("#chart1"));
+		});
+
+		var submit = new SubmitButton({
+			id: 'submit',
+			el: $('#search_btn'),
+			text: 'Update'
+		}, {tokens: true}).render();
+
     });
 });
